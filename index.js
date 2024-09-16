ccac540168607c635215740c829f65b40d3bc26d
herbertdesousa <sousaherbert138@gmail.com>
2024-09-16 07:40:02 -0300

    feat: render points on dynamically

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const GAME_SETTINGS = {
   backgroundImageSrc: "assets/img2.jpg",
+  points: [
+    { topPercentage: 0, leftPercentage: 10 },
+    { topPercentage: 50, leftPercentage: 50 },
+    { topPercentage: 90, leftPercentage: 90 },
+    { topPercentage: 0, leftPercentage: 90 },
+    { topPercentage: 90, leftPercentage: 0 },
+  ],
+};
+
+const SETUP = {
+  backgroundImageId: "#background-image",
+  containerId: "#container",
+  point: {
+    width: 32,
+    height: 32,
+    enableBackground: true,
+  },
 };
 
 class CustomImage {
@@ -22,9 +39,51 @@ class CustomImage {
   }
 }
 
+class Point {
+  classNames = [];
+
+  constructor() {}
+
+  setWidthPx(px) {
+    this.classNames.push(`w-[${px}px]`);
+  }
+
+  setBgColor(color) {
+    this.classNames.push(`bg-[${color}]`);
+  }
+
+  setHeightPx(px) {
+    this.classNames.push(`h-[${px}px]`);
+  }
+
+  setTopPercent(percentage) {
+    this.classNames.push(`top-[${percentage}%]`);
+  }
+
+  setLeftPercent(percentage) {
+    this.classNames.push(`left-[${percentage}%]`);
+  }
+
+  build() {
+    const classNames = this.classNames.join(" ");
+    return `<div class="fixed ${classNames} rounded-full"></div>`;
+  }
+}
+
 $(document).ready(() => {
-  const bgImage = new CustomImage("#background-image");
+  const bgImage = new CustomImage(SETUP.backgroundImageId);
   bgImage.setSource(GAME_SETTINGS.backgroundImageSrc);
 
-  new MouseScroll("#scroll-container");
+  for (const gamePoint of GAME_SETTINGS.points) {
+    const point = new Point();
+
+    point.setHeightPx(SETUP.point.height);
+    point.setWidthPx(SETUP.point.width);
+    point.setTopPercent(gamePoint.topPercentage);
+    point.setLeftPercent(gamePoint.leftPercentage);
+
+    if (SETUP.point.enableBackground) point.setBgColor("#f0f");
+
+    $(SETUP.containerId).append(point.build());
+  }
 });
